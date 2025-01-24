e01dfab223a37959e40f1a2dca3186cac288c6d8
maaz-salar <maazsalar42@gmail.com>
2025-01-24 14:42:35 +0500

    compete

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ getConnectionFromPool();
 
 app.use("/api/user", userRoutes);
 app.use("/api/ingrediants",ingrediantsRoutes)
-// app.use('/api/products', productRoutes);
+app.use('/api/products', productRoutes);
 app.use('/api/test',testRoutes)
 
 app.post("/api/login", (req, res) => {
@@ -40,54 +40,6 @@ app.post("/api/login", (req, res) => {
    res.send("login")
     });
 
-app.get("/api/products/search-products",  async (req, res) => {
-    try {
-        const { page = 1,search } = req.query; // Default to page 1 if not provided
-        const limit = 5;
-        const offset = (page - 1) * limit;
-      
-        // console.log(req.query, "page");
-        
-        if(search){
-            const query = `SELECT productId, prodName, prodBrand, certBody, expiryDate, disclaimer, company FROM products WHERE prodName LIKE ? LIMIT ? OFFSET ?`;
-            const [products] = await queryRunner(query, [`%${search}%`, limit, offset]);
-            
-            const countQuery = `SELECT COUNT(*) AS total_count FROM products WHERE prodName LIKE ?`;
-            const [count] = await queryRunner(countQuery, [`%${search}%`]);
-            const totalProducts = count[0].total_count;
-            
-            const totalPages = Math.ceil(totalProducts / limit);
-            
-            return res.status(200).json({
-                products,
-                nextPage: page < totalPages ? parseInt(page) + 1 : null, 
-                totalProducts,
-            });
-            }
-
-        const query = `SELECT productId, prodName, prodBrand, certBody, expiryDate, disclaimer, company FROM products LIMIT ? OFFSET ?`;
-        const [products] = await queryRunner(query, [limit, offset]);
-      
-        const countQuery = "SELECT COUNT(*) AS total_count FROM products";
-        const [count] = await queryRunner(countQuery);
-        const totalProducts = count[0].total_count;
-      
-        const totalPages = Math.ceil(totalProducts / limit);
-      
-        return res.status(200).json({
-          products,
-          nextPage: page < totalPages ? parseInt(page) + 1 : null, 
-          totalProducts,
-        });
-      } catch (error) {
-        return res.status(500).json({
-          message: "Failed to retrieve products",
-          error: error.message,
-        });
-      }
-      
-}
-);
 
 
 app.get('/', (req, res) => {
